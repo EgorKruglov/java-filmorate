8ea974d41540a74fc4ffde0d5e82c1347cd64202
EgorKruglov <120470000+EgorKruglov@users.noreply.github.com>
2023-11-15 21:59:49 +0300

    Update FilmAlreadyExistException.java

--- src/main/java/ru/yandex/practicum/filmorate/extraExceptions/FilmAlreadyExistException.java
+++ src/main/java/ru/yandex/practicum/filmorate/extraExceptions/FilmAlreadyExistException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.extraExceptions;
 
-public class FilmAlreadyExistException extends RuntimeException{
+public class FilmAlreadyExistException extends RuntimeException {
     public FilmAlreadyExistException(String message) {
         super(message);
     }
