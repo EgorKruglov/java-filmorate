450e555f5846411ae1a8d9df374f078e8ebaf594
EgorKruglov <Ovalovegor@yandex.ru>
2023-12-16 23:22:46 +0300

    add prop tables before run program

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,11 @@
+DROP TABLE IF EXISTS film_likes;
+DROP TABLE IF EXISTS friendship;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS film_genres;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS mpa;
+
 -- -----------------------------------------------------
 -- Table mpa
 -- -----------------------------------------------------
